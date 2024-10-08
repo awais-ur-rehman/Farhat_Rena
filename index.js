a781061d1cb1238bd3fd3a6e889708231179203a
awais-ur-rehman <awaisjarral37@gmail.com>
2024-10-08 11:05:31 +0500

    added: new routes for images

--- index.js
+++ index.js
@@ -7,34 +7,33 @@ const adminRoutes = require("./routes/adminRoutes");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const directOrderRoutes = require("./routes/directOrderRoutes");
+const bannerImmageRoutes = require("./routes/bannerImageRoutes");
+const categoryImmageRoutes = require("./routes/categoryImageRoutes");
+const sliderImmageRoutes = require("./routes/sliderImageRoutes");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
-// Routes
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/direct-orders", directOrderRoutes);
+app.use("/api/bannerImage", bannerImmageRoutes);
+app.use("/api/categoryImage", categoryImmageRoutes);
+app.use("/api/sliderImage", sliderImmageRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hi, This is Server!"); // Test route
+  res.send("Hi, This is Server!");
 });
 
-// Start the server
 app.listen(port, (error) => {
   if (!error) {
     console.log(`Server Running on port ${port}`);
