08f3aa257b4af4592a932c293237a94c08289000
awais-ur-rehman <awaisjarral37@gmail.com>
2024-10-22 21:49:48 +0500

    added check for base64 image size

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const sliderImmageRoutes = require("./routes/sliderImageRoutes");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
 mongoose
